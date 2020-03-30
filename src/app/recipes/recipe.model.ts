bf7205395c9215b3b8fce33b8943b760c48c7642
ksed <hydrogeo.coder@gmail.com>
2020-03-30 19:04:07 -0400

    abbreviates the recipe class

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
-  constructor(public name: string, public desc: string, public imagePath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
-  }
+  constructor(public name: string, public description: string, public imagePath: string) {}
 }
