a46f4b8fa28b83b7f72f07238c79f3b0083ca597
KSed <hydrogeo.coder@gmail.com>
2020-04-27 14:24:54 -0400

    adds ability to add and delete shopping list items and clear the shopping list form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,15 +3,16 @@ import { Ingredient } from '../shared/ingredient-model';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 50)
       ];
 
-    getIngredients() {
-        return this.ingredients.slice();
-    }
+    getIngredients() { return this.ingredients.slice(); }
+
+    getIngredient(index: number) { return this.ingredients[index]; }
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
@@ -20,6 +21,16 @@ export class ShoppingListService {
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    removeIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
